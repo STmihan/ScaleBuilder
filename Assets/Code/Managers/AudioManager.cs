07b2a4b13a0038ec66e87fb465eb172b6984c36b
Mikhail Dunaev <mih.dunaev@gmail.com>
2024-08-19 00:36:39 +0200

    Add AudioManager

---
+++ Assets/Code/Managers/AudioManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Code.Utils;
+using UnityEngine;
+
+namespace Code.Managers
+{
+    public enum SoundType
+    {
+        UITap,
+        UIIn,
+        UIOut,
+        Hit,
+        Explosion,
+        GameOver,
+    }
+
+    [Serializable]
+    public class SoundData
+    {
+        [field: SerializeField] public SoundType SoundType { get; private set; }
+        [field: SerializeField] public AudioClip AudioClip { get; private set; }
+    }
+
+    public class AudioManager : Singleton<AudioManager>
+    {
+        [SerializeField] private AudioSource _musicSource;
+        [SerializeField] private AudioSource _soundSource;
+        
+        [SerializeField] private List<SoundData> _soundClips;
+        [SerializeField] private List<AudioClip> _musicClips;
+
+        private IReadOnlyDictionary<SoundType, AudioClip> SoundClips { get; set; }
+
+        public float MusicVolume
+        {
+            get => _musicSource.volume;
+            set
+            {
+                PlayerPrefs.SetFloat("MusicVolume", value);
+                _musicSource.volume = value;
+            }
+        }
+
+        public float SoundVolume
+        {
+            get => _soundSource.volume;
+            set
+            {
+                PlayerPrefs.SetFloat("SoundVolume", value);
+                _soundSource.volume = value;
+            }
+        }
+        
+        private int _currentMusicClipIndex;
+        
+        private void Awake()
+        {
+            SoundClips = SetupSoundClips();
+            MusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+            SoundVolume = PlayerPrefs.GetFloat("SoundVolume", 0.5f);
+            StartMusic();
+        }
+        
+        public void PlaySoundOneShot(SoundType soundType)
+        {
+            _soundSource.PlayOneShot(SoundClips[soundType]);
+        }
+
+        private void StartMusic()
+        {
+            _musicSource.clip = _musicClips[0];
+            _currentMusicClipIndex = 0;
+            _musicSource.Play();
+        }
+
+        private void Update()
+        {
+            if (!_musicSource.isPlaying)
+            {
+                _currentMusicClipIndex = (_currentMusicClipIndex + 1) % _musicClips.Count;
+                _musicSource.clip = _musicClips[_currentMusicClipIndex];
+                _musicSource.Play();
+            }
+        }
+
+        private IReadOnlyDictionary<SoundType, AudioClip> SetupSoundClips()
+        {
+            var dictionary = new Dictionary<SoundType, AudioClip>();
+            foreach (var soundData in _soundClips)
+            {
+                dictionary[soundData.SoundType] = soundData.AudioClip;
+            }
+            return dictionary;
+        }
+    }
+}
--- Assets/Code/UI/SettingsUI.cs
+++ Assets/Code/UI/SettingsUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Code.Managers;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Code.UI
@@ -7,15 +8,34 @@ namespace Code.UI
     {
         private StartUI StartUI => StartUI.Instance;
         private PauseUI PauseUI => PauseUI.Instance;
+        private AudioManager AudioManager => AudioManager.Instance;
         
         [SerializeField] private Button _backButton;
+        [SerializeField] private Slider _musicSlider;
+        [SerializeField] private Slider _soundSlider;
         
         private void Start()
         {
             _backButton.onClick.AddListener(OnBackButtonClicked);
+            _musicSlider.onValueChanged.AddListener(OnMusicSliderValueChanged);
+            _soundSlider.onValueChanged.AddListener(OnSoundSliderValueChanged);
             Hide(true);
+            _soundSlider.value = AudioManager.SoundVolume;
+            _musicSlider.value = AudioManager.MusicVolume;
+        }
+
+        private void OnSoundSliderValueChanged(float arg0)
+        {
+            AudioManager.SoundVolume = arg0;
+        }
+
+        private void OnMusicSliderValueChanged(float arg0)
+        {
+            AudioManager.MusicVolume = arg0;
         }
         
+        
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
